44f7c694ad4a2a9e305f6749975bc697cde8a1fe
Mitch Rivet <mitch.rivet@gmail.com>
2015-08-14 13:17:21 -0700

    add app.php and index files

---
+++ app/app.php
@@ -0,0 +1,28 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('repeatcounter_input.html.twig');
+    });
+
+    $app->get("/counter_results", function() use ($app) {
+        $new_repeatcounter = new RepeatCounter;
+        $repeats_counted = $new_repeatcounter->countRepeats($_GET['input_string'], $_GET['word_to_count']);
+        return $app['twig']->render('repeatcounter_results.html.twig', array('result' => $repeats_counted));
+    });
+
+    return $app;
+
+?>
